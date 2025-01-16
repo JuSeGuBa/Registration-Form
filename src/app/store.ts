5be97efb886eadf505341c53354f8ff6a5b91661
sebasps532@gmail.com <you@example.comsebasps532@gmail.com>
2025-01-16 14:24:25 -0500

    Complete Redux Toolkit Setup with Persist

---
+++ src/app/store.ts
@@ -0,0 +1,36 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../counter/counterSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+// import combineReducers from "redux";
+
+const counterPersistConfig = {
+  key: "counter",
+  storage,
+};
+
+/*//Combinar Reducers si hay mas de uno
+const rootReducer = combineReducers({
+  // Añade más slices aquí si los tienes
+});*/
+
+const persistedCounterReducer = persistReducer(
+  counterPersistConfig,
+  counterReducer
+);
+
+export const store = configureStore({
+  reducer: {
+    counter: persistedCounterReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Desactiva esta comprobación para evitar errores con redux-persist
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { persistor, store } from "./app/store.ts";
+import { Counter } from "./counter/Counter.tsx";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Counter />
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
